458c8f63d05042de31d1e8e160de58ad09660b06
Filipe <filipecavalcantiximenes@gmail.com>
2024-05-25 21:02:04 -0300

    filigilst of vacs

--- src/Components/PetSelector.jsx
+++ src/Components/PetSelector.jsx
@@ -1,12 +1,12 @@
 import '../Css/PetSelector.css'
 
-export function PetSelector({handleSubmit, petsNames}){
+export function PetSelector({setPetName, petsNames}){
     
     return(
         <>
         <form>
             <section className="petSelector">
-                <select  onChange={e => handleSubmit(e)}>                  
+                <select  onChange={e => setPetName(e.target.value)}>                  
                     {
                         petsNames.map((name) => <option value={name} key={name} >{name}</option>)
                     }                             
